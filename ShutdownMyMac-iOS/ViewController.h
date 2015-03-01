336b6e9dacd1f80872489ec9a996a6a64dd39789
Jesús Bartús <jesus.bartus@gmail.com>
2015-03-01 12:17:45 +0100

    Initial Commit

---
+++ ShutdownMyMac-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShutdownMyMac-iOS
+//
+//  Created by Jesús on 1/3/15.
+//  Copyright (c) 2015 Jesús. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ShutdownMyMac-iOSTests/ShutdownMyMac_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  ShutdownMyMac_iOSTests.m
+//  ShutdownMyMac-iOSTests
+//
+//  Created by Jesús on 1/3/15.
+//  Copyright (c) 2015 Jesús. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ShutdownMyMac_iOSTests : XCTestCase
+
+@end
+
+@implementation ShutdownMyMac_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
