e4b2ea9b0a21f142fa2d3e68ba26828b0714bbfe
Jesús Bartús <jesus.bartus@gmail.com>
2015-04-26 22:15:38 +0200

    watchkit target created

---
+++ ShutdownMyMac WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  ShutdownMyMac WatchKit Extension
+//
+//  Created by Jesús on 26/4/15.
+//  Copyright (c) 2015 Jesús. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
